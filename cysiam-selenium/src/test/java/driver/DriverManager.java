a1af87d3864a4143dccacdf46f69aff50e009958
Cristian Zidarescu <cristianzidarescu@hotmail.com>
2022-12-22 18:53:03 +0200

    Add dependency in pom.xml Implemented getChromeDriverByManager

--- cysiam-selenium/src/test/java/driver/DriverManager.java
+++ cysiam-selenium/src/test/java/driver/DriverManager.java
@@ -1,5 +1,22 @@
 package driver;
 
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class DriverManager {
+
+    public static ChromeDriver getChromeDriverByManager(){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+
+        ChromeDriverManager.chromedriver().setup();
+        return new ChromeDriver(options);
+
+    }
+
+
+
 
 }
