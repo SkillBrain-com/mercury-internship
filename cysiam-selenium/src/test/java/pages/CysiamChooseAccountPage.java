f20e11cb91efbb7599076ca69cc187b54e3c7445
Ioan Stanciu <ioan.stanciu859@gmail.com>
2023-01-02 21:23:09 +0200

    refactoring

---
+++ cysiam-selenium/src/test/java/pages/CysiamChooseAccountPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CysiamChooseAccountPage {
+
+
+    @FindBy(css = "#email")
+    private WebElement username;
+
+    @FindBy(css = "#password")
+    private WebElement password;
+
+    @FindBy(css = "#next")
+    private WebElement signInButton;
+
+    public WebElement getUsername() {
+        return username;
+    }
+
+    public WebElement getPassword() {
+        return password;
+    }
+
+    public WebElement getSignInButton() {
+        return signInButton;
+    }
+}
